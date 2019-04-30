3e79c6b32fe08b8359a8e3790eb238ef274bbc31
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2019-04-30 11:20:33 -0700

    assignment4

---
+++ Assignment4/AssignFour.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class AssignFour {
+
+	public static void main(String[] args) {
+
+		Map<String, String> nameMap = new HashMap<String, String>();
+
+		// add key value pairs
+		nameMap.put("Marty", "Stepp");
+		nameMap.put("Stuart", "Reges");
+		nameMap.put("Jessica", "Miller");
+		nameMap.put("Amanda", "Camp");
+		nameMap.put("Hal", "Perkins");
+
+		Map<String, String> nameMap2 = new HashMap<String, String>();
+
+		// add key value pairs
+		nameMap2.put("Kendrick", "Perkins");
+		nameMap2.put("Stuart", "Reges");
+		nameMap2.put("Jessica", "Miller");
+		nameMap2.put("Bruce", "Reges");
+		nameMap2.put("Hal", "Perkins");
+
+		isUnique(nameMap);// returns true
+		isUnique(nameMap2);// returns false
+
+	}
+
+	public static boolean isUnique(Map<String, String> map) {
+		Set<String> set = new HashSet<String>();
+
+		for (String key : map.keySet()) {
+			String value = map.get(key);
+
+			if (set.contains(value)) {
+				System.out.println("false");
+				return false;
+			}
+
+			set.add(value);
+		}
+
+		System.out.println("true");
+		return true;
+	}
+
+}
